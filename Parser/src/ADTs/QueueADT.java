63de62d22b1701830bf77e3561682c04f9e5faf9
izaizaiza <117947026+izaizaiza@users.noreply.github.com>
2023-11-11 16:57:27 -0700

    added methods in queue adt

--- Parser/src/ADTs/QueueADT.java
+++ Parser/src/ADTs/QueueADT.java
@@ -9,6 +9,70 @@ package ADTs;
  *
  * @author izalu
  */
-public interface QueueADT {
+public interface QueueADT<E> {
+    
+    
+    
+    //methods or operations
+    /**
+     * @return an int data type; the size of the queue, i.e., how many elements
+     * are in the queue
+     */
+    public int size();
+    
+    /**
+     * @param element, with generic T data type
+     * takes element and add it at the end of the queue
+     */
+    public void enqueue(E element);
+    
+    /**
+     * removes the first element in the queue
+     */
+    public void dequeue();
+    
+    
+    /**
+     * accesses the first element in the queue without removing it
+     *
+     */
+    public void peek();
+    
+    
+    /**
+     * @param otherQueue - another QueueADT to compare with
+     * @return boolean - true or false if the two contain equal items
+     * in the same order
+     */
+    public boolean equals(QueueADT<E> otherQueue);
+    
+    
+    /**
+     * @return an iterator over the items contained in the queue
+     */
+    public Iterator<E> iterator();
+    
+    
+    /**
+     * @return  an array containing all of the items in this queue
+     */
+    public Object[] toArray( E[] queue);
+    
+    
+    /**
+     * @return a boolean to see if queue is full
+     */
+    public boolean isFull();
+    
+    /**
+     * @return a boolean to see if queue is empty
+     */
+    public boolean isEmpty();
+    
+    
+    /**
+     * clears the queue
+     */
+    public void dequeueAll();
     
 }
