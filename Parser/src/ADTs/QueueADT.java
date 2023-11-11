cfae970cb96a257fb8a1774dec2e9a76f1ce81c0
izaizaiza <117947026+izaizaiza@users.noreply.github.com>
2023-11-11 16:01:20 -0700

    setup started, need to fill code content and create the tests

---
+++ Parser/src/ADTs/QueueADT.java
@@ -0,0 +1,14 @@
+package ADTs;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author izalu
+ */
+public interface QueueADT {
+    
+}
